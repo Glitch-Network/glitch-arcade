329cc79e1662c319dac0e2c585c6a4c222486e5d
devi <133452064+infdevv@users.noreply.github.com>
2024-09-13 03:54:17 +0000

    beta

---
+++ script.js
@@ -0,0 +1,222 @@
+const data = {
+
+    "Nintendo Entertainment System": [
+        { "name": "Super Mario Bros 3", "game-image": "assets/nes/supermariobros3.png", "redirect": "nes/index.html?game=Super%20Mario%20Bros%203%20(U)%20(PRG%200)" },
+        { "name": "Super Mario Bros", "game-image": "assets/nes/supermariobros.png", "redirect": "nes/index.html?game=Super%20Mario%20Bros%20(E)" },
+        { "name": "Zelda 2: The Adventure of Link", "game-image": "assets/nes/zelda2theadventureoflink.png", "redirect": "nes/index.html?game=Zelda%202%20-%20The%20Adventure%20of%20Link%20(U)" },
+        { "name": "The Legend of Zelda", "game-image": "assets/nes/thelegendofzelda.png", "redirect": "nes/index.html?game=Legend%20of%20Zelda,%20The%20(U)%20(PRG%200)" },
+        { "name": "Mega Man", "game-image": "assets/nes/megaman.png", "redirect": "nes/index.html?game=Mega%20Man%20(U)" },
+        { "name": "Mega Man 2", "game-image": "assets/nes/megaman2.png", "redirect": "nes/index.html?game=Mega%20Man%202%20(U)" },
+        { "name": "Mega Man 3", "game-image": "assets/nes/megaman3.png", "redirect": "nes/index.html?game=Mega%20Man%203%20(U)%20[!]" },
+        { "name": "Mega Man 4", "game-image": "assets/nes/megaman4.png", "redirect": "nes/index.html?game=Mega%20Man%204%20(U)" },
+        { "name": "Mega Man 5", "game-image": "assets/nes/megaman5.png", "redirect": "nes/index.html?game=Mega%20Man%205%20(U)" },
+        { "name": "Mega Man 6", "game-image": "assets/nes/megeman6.png", "redirect": "nes/index.html?game=Mega%20Man%206%20(U)" },
+        { "name": "Metroid", "game-image": "assets/nes/metroid.png", "redirect": "nes/index.html?game=Metroid%20(U)" },
+        { "name": "Startropics 2 - Zoda's Revenge", "game-image": "assets/nes/startropics2.png", "redirect": "nes/index.html?game=Startropics%202%20-%20Zoda%27s%20Revenge%20(U)" },
+        { "name": "Snake Rattle'n Roll", "game-image": "assets/nes/snakerattlenroll.png", "redirect": "nes/index.html?game=Snake%20Rattle%27n%20Roll%20(U)" },
+        { "name": "Defender 2", "game-image": "assets/nes/defender2.png", "redirect": "nes/index.html?game=Defender%202%20(U)" },
+        { "name": "Slalom", "game-image": "assets/nes/slalom.png", "redirect": "nes/index.html?game=Slalom%20(U)" },
+        { "name": "River City Ransom", "game-image": "assets/nes/rivercityransom.png", "redirect": "nes/index.html?game=River%20City%20Ransom%20(U)" },
+        { "name": "RC Pro-Am 2", "game-image": "assets/nes/rcproam2.png", "redirect": "nes/index.html?game=RC%20Pro-Am%202%20(U)" },
+        { "name": "Prince of Persia", "game-image": "assets/nes/princeofpersia.png", "redirect": "nes/index.html?game=Prince%20of%20Persia%20(U)" },
+        { "name": "Mike Tyson's Punch-Out!!", "game-image": "assets/nes/punchout.png", "redirect": "nes/index.html?game=Mike%20Tyson%27s%20Punch-Out!!%20(U)%20(PRG%200)" },
+        { "name": "Marble Madness", "game-image": "assets/nes/marblemadness.png", "redirect": "nes/index.html?game=Marble%20Madness%20(U)" },
+        { "name": "Ghosts 'n Goblins", "game-image": "assets/nes/ghostsngoblins.png", "redirect": "nes/index.html?game=Ghosts%27n%20Goblins%20(U)" },
+        { "name": "Darkwing Duck", "game-image": "assets/nes/darkwingduck.png", "redirect": "nes/index.html?game=Darkwing%20Duck%20(U)" },
+        { "name": "Contra", "game-image": "assets/nes/contra.png", "redirect": "nes/index.html?game=Contra%20(U)" },
+        { "name": "Castlevania 3 - Dracula's Curse", "game-image": "assets/nes/castlevania3.png", "redirect": "nes/index.html?game=Castlevania%203%20-%20Dracula%27s%20Curse%20(U)" },
+        { "name": "Battletoads", "game-image": "assets/nes/battletoads.png", "redirect": "nes/index.html?game=Battletoads%20(U)" },
+        { "name": "Battletoads Double Dragon", "game-image": "assets/nes/battletoadsdoubledragon.png", "redirect": "nes/index.html?game=Battletoads%20Double%20Dragon%20(U)" },
+        { "name": "Balloon Fight", "game-image": "assets/nes/balloonfight.png", "redirect": "nes/index.html?game=Balloon%20Fight%20(E)" },
+        { "name": "1943", "game-image": "assets/nes/1943.png", "redirect": "nes/index.html?game=1943%20(U)" },
+        { "name": "Tetris 2", "game-image": "assets/nes/tetris2.png", "redirect": "nes/index.html?game=Tetris%202%20(U)%20[!]" },
+        { "name": "Terminator 2 - Judgement Day", "game-image": "assets/nes/terminator2.png", "redirect": "nes/index.html?game=Terminator%202%20-%20Judgement%20Day%20(U)" },
+        { "name": "Teenage Mutant Ninja Turtles 3", "game-image": "assets/nes/teenagemutantninjaturtles3.png", "redirect": "nes/index.html?game=Teenage%20Mutant%20Ninja%20Turtles%203%20(U)" },
+        { "name": "TaleSpin", "game-image": "assets/nes/talespin.png", "redirect": "nes/index.html?game=TaleSpin%20(U)" },
+        { "name": "Air Fortress", "game-image": "assets/nes/airfortress.png", "redirect": "nes/index.html?game=Air%20Fortress%20(U)" },
+        { "name": "Airwolf", "game-image": "assets/nes/airwolf.png", "redirect": "nes/index.html?game=Airwolf%20(U)" },
+        { "name": "Aladdin", "game-image": "assets/nes/aladdin.png", "redirect": "nes/index.html?game=Aladdin%20(E)%20[!]" },
+        { "name": "Bionic Commando", "game-image": "assets/nes/bioniccommando.png", "redirect": "nes/index.html?game=Bionic%20Commando%20(U)" },
+        { "name": "Bomberman 2", "game-image": "assets/nes/bomberman2.png", "redirect": "nes/index.html?game=Bomberman%202%20(U)" },
+        { "name": "Cobra Triangle", "game-image": "assets/nes/cobratriangle.png", "redirect": "nes/index.html?game=Cobra%20Triangle%20(U)" },
+        { "name": "Crystalis", "game-image": "assets/nes/crystalis.png", "redirect": "nes/index.html?game=Crystalis%20(U)" },
+        { "name": "Double Dragon 3 - The Sacred Stones", "game-image": "assets/nes/doubledragon3.png", "redirect": "nes/index.html?game=Double%20Dragon%203%20-%20The%20Sacred%20Stones%20(U)" },
+        { "name": "Duck Tales 2", "game-image": "assets/nes/ducktales2.png", "redirect": "nes/index.html?game=Duck%20Tales%202%20(U)" },
+        { "name": "The Fantastic Adventures of Dizzy", "game-image": "assets/nes/thefantasticadventuresofdizzy.png", "redirect": "nes/index.html?game=Fantastic%20Adventures%20of%20Dizzy,%20The%20(U)" },
+        { "name": "Galaga", "game-image": "assets/nes/galaga.png", "redirect": "nes/index.html?game=Galaga%20(U)" },
+        { "name": "Gradius", "game-image": "assets/nes/gradius.png", "redirect": "nes/index.html?game=Gradius%20(U)" },
+        { "name": "Gun Nac", "game-image": "assets/nes/gunnac.png", "redirect": "nes/index.html?game=Gun%20Nac%20(U)" },
+        { "name": "Impossible Mission 2", "game-image": "assets/nes/impossiblemission2.png", "redirect": "nes/index.html?game=Impossible%20Mission%202%20(U)" },
+        { "name": "Ninja Gaiden", "game-image": "assets/nes/ninjagaiden.png", "redirect": "nes/index.html?game=Ninja%20Gaiden%20(U)" },
+        { "name": "Ninja Gaiden 2 - The Dark Sword of Chaos", "game-image": "assets/nes/ninjagaiden2.png", "redirect": "nes/index.html?game=Ninja%20Gaiden%202%20-%20The%20Dark%20Sword%20of%20Chaos%20(U)" },
+        { "name": "Ninja Gaiden 3 - The Ancient Ship of Doom", "game-image": "assets/nes/ninjagaiden3.png", "redirect": "nes/index.html?game=Ninja%20Gaiden%203%20-%20The%20Ancient%20Ship%20of%20Doom%20(U)" },
+        { "name": "Pac-Man (Namco)", "game-image": "assets/nes/pacman.png", "redirect": "nes/index.html?game=Pac-Man%20(Namco)%20(E)%20[!]" },
+        { "name": "Paperboy 2", "game-image": "assets/nes/paperboy2.png", "redirect": "nes/index.html?game=Paperboy%202%20(U)" },
+        { "name": "Astyanax", "game-image": "assets/nes/astyanax.png", "redirect": "nes/index.html?game=Astyanax%20(U)" },
+        { "name": "Bucky O'Hare", "game-image": "assets/nes/buckyohare.png", "redirect": "nes/index.html?game=Bucky%20O%27Hare%20(U)%20[!]" },
+        { "name": "Captain America and the Avengers", "game-image": "assets/nes/captainamericaandtheavengers.png", "redirect": "nes/index.html?game=Captain%20America%20and%20the%20Avengers%20(U)" },
+        { "name": "Elite", "game-image": "assets/nes/elite.png", "redirect": "nes/index.html?game=Elite%20(E)" },
+        { "name": "Excitebike", "game-image": "assets/nes/excitebike.png", "redirect": "nes/index.html?game=Excitebike%20(E)" },
+        { "name": "Faxanadu", "game-image": "assets/nes/faxanadu.png", "redirect": "nes/index.html?game=Faxanadu%20(U)" },
+        { "name": "Lemmings", "game-image": "assets/nes/lemmings.png", "redirect": "nes/index.html?game=Lemmings%20(U)" },
+        { "name": "Q-bert", "game-image": "assets/nes/qbert.png", "redirect": "nes/index.html?game=Q-bert%20(U)" },
+        { "name": "Rampage", "game-image": "assets/nes/rampage.png", "redirect": "nes/index.html?game=Rampage%20(U)" }
+        
+    ],
+    "Nintendo GameBoy": [
+        { "name": "Batman - Return of the Joker", "game-image": "assets/gameboy/batman-returnofthejoker.png", "redirect": "/gameboy/index.html?game=Batman%20-%20Return%20of%20the%20Joker%20(USA,%20Europe)" },
+        { "name": "Donkey Kong", "game-image": "assets/gameboy/donkeykong.png", "redirect": "/gameboy/index.html?game=Donkey%20Kong%20(Japan,%20USA)%20(SGB%20Enhanced)" },
+        { "name": "DuckTales", "game-image": "assets/gameboy/ducktales.png", "redirect": "/gameboy/index.html?game=DuckTales%20(USA)" },
+        { "name": "F-15 Strike Eagle", "game-image": "assets/gameboy/f15strikeeagle.png", "redirect": "/gameboy/index.html?game=F-15%20Strike%20Eagle%20(USA,%20Europe)" },
+        { "name": "Godzilla", "game-image": "assets/gameboy/godzilla.png", "redirect": "/gameboy/index.html?game=Godzilla%20(USA,%20Europe)" },
+        { "name": "Pokemon Blue", "game-image": "assets/gameboy/pokemonblue.webp", "redirect": "/gameboy/index.html?game=pokemonblue" },
+        { "name": "Pokemon Red", "game-image": "assets/gameboy/pokemonred.webp", "redirect": "/gameboy/index.html?game=pokemonred" },
+        { "name": "Tetris", "game-image": "assets/gameboy/tetris.png", "redirect": "/gameboy/index.html?game=tetris" }
+    ],
+    "Nintendo 64": [
+        { "name": "Super Mario 64 ", "game-image": "assets/n64/supermario64.png", "redirect": "/n64/index.html?game=Super%20Mario%2064%20(U)" },
+        { "name": "Doom 64 ", "game-image": "assets/n64/doom64.png", "redirect": "/n64/index.html?game=Doom%2064%20(U)" },
+        { "name": "Dr. Mario 64 ", "game-image": "assets/n64/drmario64.png", "redirect": "/n64/index.html?game=Dr.%20Mario%2064%20(U)" },
+        { "name": "Mario Kart 64 ", "game-image": "assets/n64/mariokart64.png", "redirect": "/n64/index.html?game=Mario%20Kart%2064%20(U)" },
+        { "name": "GoldenEye 007", "game-image": "assets/n64/goldeneye007.png", "redirect": "/n64/index.html?game=GoldenEye%20007%20(U)" },
+        { "name": "Mario Party", "game-image": "assets/n64/marioparty.png", "redirect": "/n64/index.html?game=marioparty" },
+        { "name": "Super Smash Bros.", "game-image": "assets/n64/supersmashbros.png", "redirect": "/n64/index.html?game=supersmashbros" },
+
+
+          
+    ],
+    "Nintendo DS": [
+            { "name": "Tomodachi Collection", "game-image": "assets/ds/tomodachicollection.webp", "redirect": "/ds/index.html?game=tomodachicollection" },
+            { "name": "Call of Duty: Black Ops", "game-image": "assets/ds/callofduty.png", "redirect": "/ds/index.html?game=callofduty" }
+    ],
+    
+
+
+};
+
+const visibleGames = {};
+
+// Helper function to convert category names to valid CSS class names
+function convertToClassName(category) {
+    return category.replace(/\s+/g, '-');  // Replace spaces with hyphens
+}
+
+// Dynamically render categories and game boxes
+function renderCategories() {
+    const categoriesDiv = document.getElementById("categories");
+
+    for (const category in data) {
+        const className = convertToClassName(category);  // Convert category name to valid class name
+        const sectionDiv = document.createElement("div");
+        sectionDiv.classList.add("section");
+
+        const categoryTitle = document.createElement("h2");
+        categoryTitle.textContent = category;
+        sectionDiv.appendChild(categoryTitle);
+
+        const libraryRow = document.createElement("div");
+        libraryRow.classList.add("library-row");
+        sectionDiv.appendChild(libraryRow);
+
+        // Add arrow-left for navigation
+        const arrowLeft = document.createElement("div");
+        arrowLeft.classList.add("arrow-left", "hidden");
+        arrowLeft.innerHTML = "&larr;";
+        arrowLeft.onclick = () => showPrevious(category);
+        libraryRow.appendChild(arrowLeft);
+
+        // Create game boxes dynamically
+        data[category].forEach((game, index) => {
+            const gameBox = document.createElement("div");
+            gameBox.classList.add("game-box", className);  // Use the safe class name
+            gameBox.dataset.index = index;
+            gameBox.onclick = () => window.location.href = "launch.html?url=" + game.redirect + "&title=" + game.name ;  // Redirect on click
+
+            // Add game image
+            const img = new Image();
+img.src = game["game-image"];
+img.alt = game.name;
+
+// Handle successful image loading
+img.onload = () => {
+    gameBox.appendChild(img);
+};
+
+// Handle image loading errors
+img.onerror = () => {
+    img.src = "assets/notfound.webp"; // Fallback image
+    img.alt = "Image not found";
+    gameBox.appendChild(img);
+};
+
+
+            // Add game name
+            const gameTitle = document.createElement("h3");
+            gameTitle.textContent = game.name;
+            gameBox.appendChild(gameTitle);
+
+            libraryRow.appendChild(gameBox);
+        });
+
+        // Add arrow-right for navigation
+        const arrowRight = document.createElement("div");
+        arrowRight.classList.add("arrow-right");
+        arrowRight.innerHTML = "→";
+        arrowRight.onclick = () => showNext(category);
+        libraryRow.appendChild(arrowRight);
+
+        categoriesDiv.appendChild(sectionDiv);
+
+        // Initialize visibility for each category
+        visibleGames[category] = 0;
+    }
+
+    updateAllVisibility();
+}
+
+// Ensure only 5 games are visible at a time
+function updateVisibility(category) {
+    const className = convertToClassName(category);
+    const gameBoxes = document.querySelectorAll(`.${className}`);
+    const libraryRow = gameBoxes[0].parentElement;
+    const leftArrow = libraryRow.querySelector(".arrow-left");
+    const rightArrow = libraryRow.querySelector(".arrow-right");
+
+    const maxVisibleGames = 5;
+
+    gameBoxes.forEach((box) => {
+        box.classList.add("hidden");
+    });
+
+    const startIndex = visibleGames[category];
+    for (let i = startIndex; i < startIndex + maxVisibleGames; i++) {
+        if (gameBoxes[i]) {
+            gameBoxes[i].classList.remove("hidden");
+        }
+    }
+
+    leftArrow.classList.toggle("hidden", startIndex === 0);
+    rightArrow.classList.toggle("hidden", startIndex + maxVisibleGames >= data[category].length);
+}
+
+function showNext(category) {
+    const maxVisibleGames = 5;
+    visibleGames[category] = Math.min(visibleGames[category] + maxVisibleGames, data[category].length - maxVisibleGames);
+    updateVisibility(category);
+}
+
+function showPrevious(category) {
+    const maxVisibleGames = 5;
+    visibleGames[category] = Math.max(visibleGames[category] - maxVisibleGames, 0);
+    updateVisibility(category);
+}
+
+window.addEventListener("resize", updateAllVisibility);
+
+function updateAllVisibility() {
+    for (const category in data) {
+        updateVisibility(category);
+    }
+}
+
+renderCategories();
